68f17fe298fffbaa052497b65bab5eafe37a4472
Victor Costa <victorcrcosta@gmail.com>
2024-07-25 18:44:15 +0100

    Add docstring to main function

--- doc_parser/file_parser.py
+++ doc_parser/file_parser.py
@@ -3,8 +3,22 @@ from bs4 import BeautifulSoup
 
 # CURRENT METHOD: ==================================================================== //
 # –– Excludes tables.
-def parse_file(file: str) -> list[str]:
-    soup = BeautifulSoup(file, "html.parser")
+def parse_html_from_docx(html: str) -> list[str]:
+    """
+    Parses an html string, that has a `.docx` as source, and extracts its text
+    passages.
+    Currently, it ignores tables and extracts.
+    
+    The 'docx --> html' conversion is usually done by the 'mammoth' library
+    (https://github.com/mwilliamson/python-mammoth).
+
+    Args:
+        html (str): The html string to be parsed.
+
+    Returns:
+        list[str]: A list of passages extracted from the html.
+    """
+    soup = BeautifulSoup(html, "html.parser")
     passages = []
     for child in soup.children:
         if child.name == "table":
