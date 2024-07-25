c02748d4421b3fda2b671ec8eb12b5d211b969f3
Victor Costa <victorcrcosta@gmail.com>
2024-07-25 17:46:24 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# HTML into Word doc Parser
---
+++ doc_parser/file_parser.py
@@ -0,0 +1,17 @@
+from bs4 import BeautifulSoup
+
+
+# CURRENT METHOD: ==================================================================== //
+# –– Excludes tables.
+def parse_file(file: str) -> list[str]:
+    soup = BeautifulSoup(file, "html.parser")
+    passages = []
+    for child in soup.children:
+        if child.name == "table":
+            passages.append("[TABELA]")
+        elif child.name == "ol":
+            for grandchild in child.children:
+                passages.append(grandchild.text)
+        else:
+            passages.append(child.text)
+    return passages
